f7c4e22a6879a1bc8a1bbafe177c4e65b4556bf6
Santiago <s.tenjov@uniandes.edu.co>
2023-11-28 07:57:16 -0500

    first commit

---
+++ src/main/java/com/example/Hotel_Andes_mongoDB/HotelAndesMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hotel_Andes_mongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAndesMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelAndesMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hotel_Andes_mongoDB/HotelAndesMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotel_Andes_mongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAndesMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
